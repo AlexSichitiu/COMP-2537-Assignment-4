30ce624fee25a0bbb6f2f9870cfe6247c5081d94
AlexSichitiu <asichitiu@gmail.com>
2023-05-16 09:49:00 -0700

    Changed easy mode time limit to 120s

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let candidatePokemon = [];
 let difficulty = 0;
-let difficultyMap = [{cards:3, time:18}, {cards:6, time:90}, {cards:12, time:60}];
+let difficultyMap = [{cards:3, time:120}, {cards:6, time:90}, {cards:12, time:60}];
 var timer, pairManager;
 var clickCount = 0;
 var poweredUp = false;
